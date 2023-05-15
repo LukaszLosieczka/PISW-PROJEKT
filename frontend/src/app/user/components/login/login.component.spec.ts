7fe3115e833602aaeba64dfa5910c07dba84289f
Łukasz Łosieczka <lukasz.losieczka@gmail.com>
2023-05-15 20:09:44 +0200

    tests fixes

--- frontend/src/app/user/components/login/login.component.spec.ts
+++ frontend/src/app/user/components/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {TicketsModule} from "../../../tickets/tickets.module";
+import {AppRoutingModule} from "../../../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +13,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        RouterModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
--- frontend/src/app/user/components/register/register.component.spec.ts
+++ frontend/src/app/user/components/register/register.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {TicketsModule} from "../../../tickets/tickets.module";
+import {AppRoutingModule} from "../../../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,6 +13,11 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        RouterModule],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
