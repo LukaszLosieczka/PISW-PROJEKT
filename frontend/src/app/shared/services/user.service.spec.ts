03adeb3748f72701c1388642ab9e8fbb3b401cb8
Łukasz Łosieczka <lukasz.losieczka@gmail.com>
2023-06-21 18:04:45 +0200

    tests error fixes

--- frontend/src/app/shared/services/user.service.spec.ts
+++ frontend/src/app/shared/services/user.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import {BrowserModule} from "@angular/platform-browser";
+import {TicketsModule} from "../../tickets/tickets.module";
+import {AppRoutingModule} from "../../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        RouterModule]
+    });
     service = TestBed.inject(UserService);
   });
 
--- frontend/src/app/tickets/resolvers/history-tickets.resolver.spec.ts
+++ frontend/src/app/tickets/resolvers/history-tickets.resolver.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HistoryTicketsResolver } from './history-tickets.resolver';
+import {BrowserModule} from "@angular/platform-browser";
+import {TicketsModule} from "../tickets.module";
+import {AppRoutingModule} from "../../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 describe('HistoryTicketsResolver', () => {
   let resolver: HistoryTicketsResolver;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        TicketsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        RouterModule]
+    });
     resolver = TestBed.inject(HistoryTicketsResolver);
   });
 
